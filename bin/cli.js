98b3dd20ef676f455529e8feca807d5c4d643bb2
yuchao.feng <yuchao.feng@yiducloud.cn>
2020-10-14 17:41:07 +0800

    update

--- bin/cli.js
+++ bin/cli.js
@@ -5,6 +5,50 @@ const path = require('path');
 
 const inquirer = require('inquirer');
 
+const stat = fs.stat;
+
+const copy = function (src, dst) {
+    //读取目录
+    fs.readdir(src, function (err, paths) {
+        console.log(paths);
+        if (err) {
+            throw err;
+        }
+        paths.forEach(function (path) {
+            let _src = src + '/' + path;
+            let _dst = dst + '/' + path;
+            let readable;
+            let writable;
+            stat(_src, function (err, st) {
+                if (err) {
+                    throw err;
+                }
+
+                if (st.isFile()) {
+                    readable = fs.createReadStream(_src);//创建读取流
+                    writable = fs.createWriteStream(_dst);//创建写入流
+                    readable.pipe(writable);
+                } else if (st.isDirectory()) {
+                    exists(_src, _dst, copy);
+                }
+            });
+        });
+    });
+};
+
+const exists = function (src, dst, callback) {
+    //测试某个路径下文件是否存在
+    fs.exists(dst, function (exists) {
+        if (exists) {//不存在
+            callback(src, dst);
+        } else {//存在
+            fs.mkdir(dst, function () {//创建目录
+                callback(src, dst)
+            })
+        }
+    })
+}
+
 inquirer.prompt([
     {
         type: 'input',
@@ -12,13 +56,8 @@ inquirer.prompt([
         message: 'your project name?'
     }
 ]).then(answer => {
-    const tmplDir = path.join(__dirname, 'templates');
+    const tmplDir = path.join(__dirname, '../templates');
     const destDir = process.cwd();
 
-    fs.readdir(tmplDir, (err, files) => {
-        if (err) throw err;
-        files.forEach(file => {
-            fs.writeFileSync(path.join(destDir, file), file);
-        })
-    });
+    exists(tmplDir, destDir, copy)
 });
